5bc07818acc454f37b3a0fb644a1849505c932ce
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-08-27 18:10:07 -0600

    Add week 1 demo

---
+++ app/week1/page.js
@@ -0,0 +1,10 @@
+export default function Week1() {
+  return (
+    <main>
+      <h1>Week 1 Demo</h1>
+      <p>
+        Congratulations on correctly setting up your development environment!
+      </p>
+    </main>
+  );
+}
