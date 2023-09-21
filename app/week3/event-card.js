2567326a7a08f4958d164ecd20f5c4d89afa1718
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-09-21 08:50:42 -0600

    clean up demo a little

--- app/week3/event-card.js
+++ app/week3/event-card.js
@@ -1,6 +1,6 @@
 export default function EventCard({ name, date, location }) {
   return (
-    <div className="border border-sky-500 bg-sky-800 w-96 m-4 p-2">
+    <div className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2">
       <h3 className="text-xl font-bold">{name}</h3>
       <p>Date: {date}</p>
       <p>Location: {location}</p>
--- app/week3/weather.js
+++ app/week3/weather.js
@@ -1,19 +1,23 @@
 import Link from "next/link";
+import Heading from "./heading";
 
 export default function Weather() {
   return (
-    <>
-      <div className="w-48 ml-8 p-2">
+    <div>
+      <Heading title="Weather" />
+
+      <div className="ml-8 p-2">
         <p>Temperature: 15°C</p>
         <p>
           <Link
             href="https://www.theweathernetwork.com/ca/weather/alberta/calgary"
             className="underline text-sky-400"
+            target="_blank"
           >
             Weather forecast
           </Link>
         </p>
       </div>
-    </>
+    </div>
   );
 }
