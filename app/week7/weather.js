32e65eeadf4b52245e73f0598130e539ac1fbfda
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-10-16 07:01:52 -0600

    Improve Week 7 demo code

--- app/week7/weather.js
+++ app/week7/weather.js
@@ -4,6 +4,14 @@ import Heading from "./heading";
 
 import { useState, useEffect } from "react";
 
+async function getWeather() {
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=51.064&longitude=-114.08&current=temperature_2m,is_day,precipitation,rain,showers,snowfall,cloudcover&timezone=auto`
+  );
+  const data = await response.json();
+  return data;
+}
+
 export default function Weather() {
   const [weather, setWeather] = useState(null);
 
@@ -11,11 +19,7 @@ export default function Weather() {
     // This is an IIFE (Immediately Invoked Function Expression)
     (async () => {
       try {
-        // <-- Try to fetch the data}
-        const response = await fetch(
-          `https://api.open-meteo.com/v1/forecast?latitude=51.064&longitude=-114.08&current=temperature_2m,is_day,precipitation,rain,showers,snowfall,cloudcover&timezone=auto`
-        );
-        const data = await response.json();
+        const data = await getWeather(); // <-- Try to fetch the data}
         setWeather(data);
       } catch (error) {
         console.error(error);
@@ -23,31 +27,34 @@ export default function Weather() {
     })(); // <-- Invoke the anonymous function immediately
 
     return () => {
-      // <-- Clean up any side effects
+      // <-- Clean up any side effects, if necessary
+      // E.g. remove event listeners, cancel intervals or timeouts, etc.
+      // This function is called when the component is unmounted
+      // (i.e. when the user navigates away from the page)
     };
   }, []); // <-- Empty array: Run the effect only once after the initial render
+
+  // Destructure the data and rename
+  const { temperature_2m: temp, cloudcover: cloudCover } =
+    weather?.current || {};
+  const { temperature_2m: tempUnit, cloudcover: cloudCoverUnit } =
+    weather?.current_units || {};
 
   return (
     <div>
       <Heading title="Current Weather" />
 
       <div className="ml-8 p-2">
-        {/* the ? is optional chaining */}
         <p>
           Temperature:{" "}
-          {weather &&
-          weather.current?.temperature_2m &&
-          weather.current_units?.temperature_2m
-            ? weather.current.temperature_2m +
-              weather.current_units.temperature_2m
+          {temp !== null && temp !== undefined && tempUnit
+            ? `${temp}${tempUnit}`
             : "unavailable"}
         </p>
         <p>
           Cloud cover:{" "}
-          {weather &&
-          weather.current?.cloudcover &&
-          weather.current_units?.cloudcover
-            ? weather.current.cloudcover + weather.current_units.cloudcover
+          {cloudCover !== null && cloudCover !== undefined && cloudCoverUnit
+            ? `${cloudCover}${cloudCoverUnit}`
             : "unavailable"}
         </p>
       </div>
