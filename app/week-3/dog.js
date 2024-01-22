65de9483e74f9a0edf6c4e81d1b5bbb797a457c5
Aaron Warsylewicz <aaron@warsylewicz.ca>
2024-01-22 12:51:24 -0700

    Add Week 3 Demo

--- app/week-3/dog.js
+++ app/week-3/dog.js
@@ -1,13 +1,10 @@
-export default function Dog({ name, description, imageURL }) {
+export default function Dog({ name, breed, age, color }) {
   return (
-    <div>
-      <h2 className="text-xl text-blue-600">{name}</h2>
-      <p className="ml-2">{description}</p>
-      <img
-        className="w-64 h-64 object-cover rounded-lg mt-2"
-        src={imageURL}
-        alt={name}
-      ></img>
+    <div className="ml-4">
+      <h2 className="text-green-500">{name}</h2>
+      <p className="text-xs">
+        {breed}, {age} years old, {color}
+      </p>
     </div>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,38 +2,48 @@ import Dog from "./dog";
 
 export default function Page() {
   let dog1 = {
-    name: "German Shepherd",
-    description:
-      "German Shepherds are very intelligent dogs. They are also very protective.",
-    imageURL:
-      "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074_960_720.jpg", // This is a sample link.
+    name: "Fluffy",
+    breed: "Poodle",
+    age: 4,
+    color: "white",
   };
 
   let dog2 = {
-    name: "Beagle",
-    description: "Beagles are very energetic dogs. They are also very playful.",
-    imageURL:
-      "https://cdn.pixabay.com/photo/2016/02/19/10/16/beagle-1209841_960_720.jpg", // This is a sample link.
+    name: "Spot",
+    breed: "Dalmatian",
+    age: 3,
+    color: "black and white",
   };
 
   let dog3 = {
-    name: "Labrador",
-    description: "Labradors are very friendly dogs. They are also very loyal.",
-    imageURL:
-      "https://cdn.pixabay.com/photo/2016/02/19/11/23/dog-1209934_960_720.jpg", // This is a sample link.
+    name: "Rover",
+    breed: "Golden Retriever",
+    age: 2,
+    color: "golden",
   };
 
   return (
-    <main className="m-4">
-      <h1 className="text-6xl font-bold text-red-500 mb-8">
-        My Favourite Dog Breeds
-      </h1>
-      <p className="mb-2">Here are my favourite dog breeds:</p>
-      <div className="ml-3">
-        <Dog {...dog1} />
-        <Dog {...dog2} />
-        <Dog {...dog3} />
-      </div>
+    <main className="m-4 ml-6">
+      <h1 className="text-4xl font-bold">Week 3</h1>
+      <p>These are my favourite dogs.</p>
+      <Dog
+        name={dog1.name}
+        breed={dog1.breed}
+        age={dog1.age}
+        color={dog1.color}
+      />
+      <Dog
+        name={dog2.name}
+        breed={dog2.breed}
+        age={dog2.age}
+        color={dog2.color}
+      />
+      <Dog
+        name={dog3.name}
+        breed={dog3.breed}
+        age={dog3.age}
+        color={dog3.color}
+      />
     </main>
   );
 }
