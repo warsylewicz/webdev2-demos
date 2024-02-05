0f9546b6b8fb7505636b73fb243ca1f7f4fc461d
Aaron Warsylewicz <aaron@warsylewicz.ca>
2024-02-05 13:32:20 -0700

    Week 5 Demo

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,53 +1,58 @@
 "use client";
 
 export default function Page() {
-  let dogList = [
+  const dogs = [
     {
       id: 1,
-      name: "Sad dog",
-      description: "This dog is sad",
+      name: "Corgnelius",
+      breed: "Corgi",
       imageUrl:
-        "https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&w=260&h=750&dpr=2",
     },
     {
       id: 2,
-      name: "Happy dog",
-      description: "This happy dog is fluffy.",
+      name: "Sleepy",
+      breed: "Brittany",
       imageUrl:
-        "https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/731022/pexels-photo-731022.jpeg?auto=compress&cs=tinysrgb&w=260&h=750&dpr=2",
     },
     {
       id: 3,
-      name: "Toque dog",
-      description: "This dog is wearing a toque.",
+      name: "Smiley",
+      breed: "Cute",
       imageUrl:
-        "https://images.pexels.com/photos/4588052/pexels-photo-4588052.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/247937/pexels-photo-247937.jpeg?auto=compress&cs=tinysrgb&w=260&h=750&dpr=2",
     },
   ];
 
-  dogList.sort((a, b) => a.name.localeCompare(b.name));
+  // sort the dogs by name
+  dogs.sort((a, b) => a.name.localeCompare(b.name));
+  dogs.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
 
-  // filter list of dogs so that we don't have dog with id == 2
-  dogList = dogList.filter((dog) => dog.id !== 2);
+  // do not show dog with id == 2
+  const filteredDogs = dogs.filter((dog) => dog.id !== 2);
+  console.log(filteredDogs);
 
-  console.log("Where is this?");
-
-  function handleClick(e) {
-    alert(e.target.innerText);
-  }
+  const handleClick = (id) => {
+    alert(id);
+  };
 
   return (
-    <main>
-      <h1>My Dog List</h1>
+    <div className="sm:bg-slate-500 md:bg-slate-200 bg-slate-600">
+      <h1>Week 5 Dog Demo</h1>
       <ul>
-        {dogList.map((dog) => (
-          <li key={dog.id} onClick={handleClick}>
+        {dogs.map((dog) => (
+          <li key={dog.id} onClick={() => handleClick(dog.id)}>
             <h2>{dog.name}</h2>
-            <p>{dog.description}</p>
+            <p>{dog.breed}</p>
             <img src={dog.imageUrl} alt={dog.name} />
           </li>
         ))}
       </ul>
-    </main>
+    </div>
   );
 }
