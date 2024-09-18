c675a679935d3cb911186c05f0b77f718aea01a2
Aaron Warsylewicz <aaron@warsylewicz.ca>
2024-09-18 12:21:08 -0600

    Week 3 Demo complete

--- app/week-3/dog.js
+++ app/week-3/dog.js
@@ -1,10 +1,12 @@
-export default function Dog({ name, breed, age, color }) {
+export default function Dog({ name, breed, age, colour }) {
   return (
-    <div className="ml-4">
-      <h2 className="text-green-500">{name}</h2>
-      <p className="text-xs">
-        {breed}, {age} years old, {color}
+    <section className="m-2 bg-neutral-100">
+      <h3 className="text-xl font-bold text-violet-900 ">{name}</h3>
+      <p>
+        <span className="font-bold">Breed:</span> {breed}
       </p>
-    </div>
+      <p>Age: {age}</p>
+      <p>Colour: {colour}</p>
+    </section>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,47 +2,46 @@ import Dog from "./dog";
 
 export default function Page() {
   let dog1 = {
-    name: "Fluffy",
-    breed: "Poodle",
-    age: 4,
-    color: "white",
+    name: "Buddy",
+    breed: "Australian Shepherd",
+    age: 11,
+    colour: "brown",
   };
 
   let dog2 = {
-    name: "Spot",
-    breed: "Dalmatian",
-    age: 3,
-    color: "black and white",
+    name: "Dingo",
+    breed: "Miniature Schnauzer",
+    age: 7,
+    colour: "black",
   };
 
   let dog3 = {
-    name: "Rover",
-    breed: "Golden Retriever",
-    age: 2,
-    color: "golden",
+    name: "Ginger",
+    breed: "Sausage Dog",
+    age: 1,
+    colour: "ginger",
   };
 
   return (
-    <main className="m-4 ml-6">
-      <h1 className="text-4xl font-bold">Week 3</h1>
-      <p>These are my favourite dogs.</p>
+    <main className="m-4">
+      <h2 className="text-2xl font-bold">Meet the Dogs</h2>
       <Dog
         name={dog1.name}
         breed={dog1.breed}
         age={dog1.age}
-        color={dog1.color}
+        colour={dog1.colour}
       />
       <Dog
         name={dog2.name}
         breed={dog2.breed}
         age={dog2.age}
-        color={dog2.color}
+        colour={dog2.colour}
       />
       <Dog
         name={dog3.name}
         breed={dog3.breed}
         age={dog3.age}
-        color={dog3.color}
+        colour={dog3.colour}
       />
     </main>
   );
