25c8456e1e536ea50d5c5b1fc53ec0da4925c68e
Aaron Warsylewicz <aaron@warsylewicz.ca>
2024-09-25 12:33:13 -0600

    Week 4 demo finished

--- app/week-4/counter.js
+++ app/week-4/counter.js
@@ -5,22 +5,18 @@ import { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  // count is the current value of the state
-  // setCount is a function that updates the state
-
-  const increment = () => setCount(count + 1);
-
-  // never do this to increment count
-  // const increment = () => count++;
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
 
   return (
     <div>
-      <p>counter: {count}</p>
-      {count > 5 && <p>counter is greater than 5</p>}
-      {count === 3 ? <p>counter is 3</p> : <p>counter is not 3</p>}
+      <p>Count: {count}</p>
       <button
         onClick={increment}
-        className="w-24 bg-blue-600 rounded-md hover:bg-blue-300 hover:font-bold active:bg-yellow-300"
+        className="bg-blue-500 hover:bg-blue-700 active:bg-red-300 rounded
+        w-32"
       >
         Increment
       </button>
