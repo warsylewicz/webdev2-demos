99bd79642e23db62fc98847fb98c10c6b7733162
Aaron Warsylewicz <aaron@warsylewicz.ca>
2025-02-05 13:30:19 -0700

    Week 5 Demo

--- app/week-5/dog-form.js
+++ app/week-5/dog-form.js
@@ -7,34 +7,11 @@ export default function DogForm() {
   const [breed, setBreed] = useState("");
   const [age, setAge] = useState(0);
 
-  const handleNameChange = (event) => {
-    let name = event.target.value;
-    name = name.replace(/[^a-zA-Z\s]/g, "");
-    setName(name);
-  };
-
-  const handleBreedChange = (event) => {
-    let breed = event.target.value;
-    breed = breed.replace(/[^a-zA-Z\s]/g, "");
-    setBreed(breed);
-  };
-
-  const handleAgeChange = (event) => {
-    let age = event.target.value;
-    age = parseInt(age);
-    if (isNaN(age)) {
-      age = 0;
-    }
-    if (age < 0) {
-      age = 0;
-    }
-    setAge(age);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     let dog = { name, breed, age };
-    console.log(dog); // eventually save to a database
+    console.log(dog);
+    // in Week 10 we will save this to a database
 
     // reset form
     setName("");
@@ -42,45 +19,62 @@ export default function DogForm() {
     setAge(0);
   };
 
+  const handleNameChange = (event) => {
+    const newName = event.target.value;
+    if (newName.length > 10) {
+      // alert("Name must be 10 characters or less");
+      return;
+    }
+    setName(event.target.value);
+  };
+
+  const handleBreedChange = (event) => {
+    let newBreed = event.target.value;
+    newBreed = newBreed.toUpperCase();
+    setBreed(newBreed);
+  };
+
+  const handleAgeChange = (event) => {
+    let newAge = event.target.value;
+    let ageNum = parseInt(newAge);
+    if (ageNum < 0) {
+      return;
+    }
+    setAge(ageNum);
+  };
+
   return (
-    <div className="m-2">
-      <h2 className="text-2xl">Add a Dog</h2>
-      <form className="m-2" onSubmit={(event) => handleSubmit(event)}>
-        <label htmlFor="name">Name:</label>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
         <input
-          id="name"
           type="text"
+          id="name"
           value={name}
           onChange={(event) => handleNameChange(event)}
-          className="border border-black m-2"
         />
-        <label htmlFor="breed">Breed:</label>
+        <label htmlFor="breed">Breed</label>
         <input
-          id="breed"
           type="text"
+          id="breed"
           value={breed}
           onChange={(event) => handleBreedChange(event)}
-          className="border border-black m-2"
         />
-        <label htmlFor="age">Age:</label>
+        <label htmlFor="age">Age</label>
         <input
-          id="age"
           type="number"
+          id="age"
           value={age}
           onChange={(event) => handleAgeChange(event)}
-          className="border border-black m-2"
         />
-        <input
-          type="submit"
-          value="Add Dog"
-          className="m-2 bg-blue-500 hover:bg-blue-700"
-        />
+        <button type="submit">Submit</button>
       </form>
-      <div className="text-lg">
-        <p>Name: {name.length >= 3 ? name : "Name too short"}</p>
-        <p>Breed: {breed}</p>
-        <p>Age: {age}</p>
+      <div>
+        {name.length === 10 && <p>Name must be 10 characters or less</p>}
+        <p>
+          The dog is {age} {age === 1 ? "year" : "years"} old.
+        </p>
       </div>
-    </div>
+    </section>
   );
 }
