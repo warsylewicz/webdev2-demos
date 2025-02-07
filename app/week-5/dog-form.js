bb22c8394c0908b6e26da7392b8fe5246c980c4f
Aaron Warsylewicz <aaron@warsylewicz.ca>
2025-02-07 13:39:27 -0700

    Week 5 Demos Friday

--- app/week-5/dog-form.js
+++ app/week-5/dog-form.js
@@ -8,73 +8,51 @@ export default function DogForm() {
   const [age, setAge] = useState(0);
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // do not submit the form or reload the page
     let dog = { name, breed, age };
     console.log(dog);
-    // in Week 10 we will save this to a database
+    // save to database in week 10
 
-    // reset form
+    // now clear the form
     setName("");
     setBreed("");
     setAge(0);
   };
 
-  const handleNameChange = (event) => {
-    const newName = event.target.value;
-    if (newName.length > 10) {
-      // alert("Name must be 10 characters or less");
-      return;
-    }
-    setName(event.target.value);
+  const handleNameChange = (e) => {
+    // no special characters allowed
+    let newName = e.target.value;
+    newName = newName.replace(/[^a-zA-Z ]/g, "");
+    setName(newName);
   };
 
-  const handleBreedChange = (event) => {
-    let newBreed = event.target.value;
-    newBreed = newBreed.toUpperCase();
-    setBreed(newBreed);
-  };
-
-  const handleAgeChange = (event) => {
-    let newAge = event.target.value;
-    let ageNum = parseInt(newAge);
-    if (ageNum < 0) {
-      return;
-    }
-    setAge(ageNum);
+  const handleBreedChange = (e) => {
+    setBreed(e.target.value);
   };
 
   return (
     <section>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
+        <label>Name</label>
+        <input type="text" value={name} onChange={(e) => handleNameChange(e)} />
+        <label>Breed</label>
         <input
           type="text"
-          id="name"
-          value={name}
-          onChange={(event) => handleNameChange(event)}
-        />
-        <label htmlFor="breed">Breed</label>
-        <input
-          type="text"
-          id="breed"
           value={breed}
-          onChange={(event) => handleBreedChange(event)}
+          onChange={(e) => handleBreedChange(e)}
         />
-        <label htmlFor="age">Age</label>
+        <label>Age</label>
         <input
           type="number"
-          id="age"
           value={age}
-          onChange={(event) => handleAgeChange(event)}
+          onChange={(e) => setAge(parseInt(e.target.value))}
         />
         <button type="submit">Submit</button>
       </form>
       <div>
-        {name.length === 10 && <p>Name must be 10 characters or less</p>}
-        <p>
-          The dog is {age} {age === 1 ? "year" : "years"} old.
-        </p>
+        {breed.match(/[^a-zA-Z ]/) ? "Breed contains special characters" : ""}
       </div>
+      <div>{age === 0 ? "Age is required" : ""}</div>
     </section>
   );
 }
